ef3aeff1840c605ba0319da482665211867110ef
Krishnapatel129 <143897864+Krishnapatel129@users.noreply.github.com>
2026-05-22 07:53:37 +0530

    Day1 Task

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="bg-black h-screen flex items-center justify-center">
+      <h1 className="text-white text-6xl font-bold">
+        Speech to Text APP
+      </h1>
+    </div>
+  )
+}
+
+export default App
