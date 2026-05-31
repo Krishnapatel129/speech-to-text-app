22ea392992a005594b0d1600f2ff356d89091305
Krishnapatel129 <143897864+Krishnapatel129@users.noreply.github.com>
2026-05-31 21:29:56 +0530

    Fix backend errors

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,17 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: { origin: "*" }
 });
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204);
+  }
+
+  next();
+});
 app.get("/", (req, res) => res.send("OK"));
 app.get("/transcriptions", (req, res) => {
   res.json([]);
