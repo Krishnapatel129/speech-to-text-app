6cae510475dffe5a3e3f2900003202f03fd986df
Krishnapatel129 <143897864+Krishnapatel129@users.noreply.github.com>
2026-05-31 20:01:00 +0530

    socket update

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,5 +1,18 @@
 import { io } from "socket.io-client";
 
-const socket = io("https://speech-to-text-app-1.onrender.com");
+const socket = io(
+  "https://speech-to-text-app-1.onrender.com",
+  {
+    transports: ["websocket"],
+  }
+);
+
+socket.on("connect", () => {
+  console.log("Connected:", socket.id);
+});
+
+socket.on("connect_error", (err) => {
+  console.error("Socket Error:", err.message);
+});
 
 export default socket;
