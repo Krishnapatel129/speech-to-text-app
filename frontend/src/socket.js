6ae14a8b27fb2e161ea34561c1f5bea56f22b763
Krishnapatel129 <143897864+Krishnapatel129@users.noreply.github.com>
2026-05-31 20:57:21 +0530

    Fix backend errors

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("https://speech-to-text-app-1.onrender.com", {
+const socket = io("https://speech-to-text-app-1-h8o2.onrender.com", {
   transports: ["polling", "websocket"],
   secure: true,
   reconnection: true,
